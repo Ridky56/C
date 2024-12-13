5b8c8904ad895e85270709fda60ca30c1cac81a8
Ridky <ridkisuero56@gmail.com>
2024-12-13 06:49:25 -0600

    commit inicial

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include "cs50.h"
+#include <stdio.h>
+
+int main(void)
+{
+
+  // este es un comentario
+    string name = get_string("What's your name? ");
+    printf("hello, %s!\n", name);
+    return 0; 
+}
+   
